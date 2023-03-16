9b7cc25a9bf2e7a52048c37a029446915b763850
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-16 12:05:58 +0600

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        int[] winter = {12, 1, 2};
+		int[] spring = {3, 4, 5};
+		int[] summer = {6, 7, 8};
+		int[] autumn = {9, 10, 11};
+		for (int monthInt:winter) {
+			if (integerNum == monthInt) {
+				System.out.print("Winter");
+			}
+		}
+		for (int monthInt:spring) {
+			if (integerNum == monthInt) {
+				System.out.print("Spring");
+			}
+		}
+		for (int monthInt:summer) {
+			if (integerNum == monthInt) {
+				System.out.print("Summer");
+			}
+		}
+		for (int monthInt:autumn) {
+			if (integerNum == monthInt) {
+				System.out.print("Autumn");
+			}
+		}
+		if (integerNum <= 0 || integerNum >= 13) {
+			System.out.print("Wrong input");
+		}
     }
 }
