c3322ab2530469e3e381d88f8f066df396a5af29
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-16 12:13:50 +0600

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,55 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month == 1) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 2) {
+			System.out.println(28 + "or" + 29);
+			return month;
+		}
+		if (month == 3) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 4) {
+			System.out.println(30);
+			return month;
+		}
+		if (month == 5) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 6) {
+			System.out.println(30);
+			return month;
+		}
+		if (month == 7) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 8) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 9) {
+			System.out.println(30);
+			return month;
+		}
+		if (month == 10) {
+			System.out.println(31);
+			return month;
+		}
+		if (month == 11) {
+			System.out.println(30);
+			return month;
+		}
+		if (month == 12) {
+			System.out.println(31);
+			return month;
+		} else {
+			System.out.println("wrong number!");
+		}
     }
 }
