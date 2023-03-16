4ac3a6b093adbd0c142d968fdb7e4dee5028755b
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-16 13:22:12 +0600

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,22 +3,22 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary <= 10000) {
-			double afterTaxes = salary * (1 - 0.15);
-			int afterTaxesX = (int) afterTaxes;
-			System.out.println(afterTaxesX);
-		}
-		if (10000 < salary && salary <= 20000) {
-			double afterTaxes = salary * (1 - 0.18);
-			int afterTaxesX = (int) afterTaxes;
-			System.out.println(afterTaxesX);
-		}
-		if (salary > 20000) {
-			double afterTaxes = salary * (1 - 0.20);
-			int afterTaxesX = (int) afterTaxes;
-			System.out.println(afterTaxesX);
-		}
-		if (salary < 0) {
-			System.out.println("wrong input!");
-		}
+	    double afterTaxes = salary * (1 - 0.15);
+	    int afterTaxesX = (int) afterTaxes;
+	    System.out.println(afterTaxesX);
+	}
+	if (10000 < salary && salary <= 20000) {
+	    double afterTaxes = salary * (1 - 0.18);
+	    int afterTaxesX = (int) afterTaxes;
+	    System.out.println(afterTaxesX);
+	}
+	if (salary > 20000) {
+	    double afterTaxes = salary * (1 - 0.20);
+	    int afterTaxesX = (int) afterTaxes;
+	    System.out.println(afterTaxesX);
+	}
+	if (salary < 0) {
+	    System.out.println("wrong input!");
+	}
     }
 }
