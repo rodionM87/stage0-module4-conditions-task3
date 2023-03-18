c97dad49fe0c7e29fb9948702ca9b9e09df0741f
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-18 22:35:21 +0600

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,13 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-        char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	for (char letter:alpha) {
-	    if (letter == symbol) {
-		System.out.println("English");
-	    } else {
-		System.out.println("Non English");
+        Boolean checkVar = checkAlpha(symbol);
+	if (checkVar == true) {
+	    System.out.println("English");
+	}
+	else if (checkVar == false) {
+	    System.out.println("Non English");
+	
+    }
+    public static Boolean checkAlpha(char symbol) {
+	char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	boolean boolVar = false;
+	for (char tempChar:alpha) {
+	    if (tempChar == symbol) {
+		boolVar = true;
 	    }
 	}
-    }
+	return boolVar;
 }
