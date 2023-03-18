f1134282e7b973a7f7ece27e878ccff3554837d1
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-18 22:46:08 +0600

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -8,7 +8,7 @@ public class IsEnglishSymbolDeterminer {
 	}
 	else if (checkVar == false) {
 	    System.out.println("Non English");
-	
+	}
     }
     public static Boolean checkAlpha(char symbol) {
 	char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
@@ -19,4 +19,5 @@ public class IsEnglishSymbolDeterminer {
 	    }
 	}
 	return boolVar;
+    }
 }
